2a9df0347a1ac6476decba5e2e57de3f20e8749e
Ydla-chan <Aldyjhonatanhutasoit.1@gmail.com>
2024-11-26 15:38:58 +0700

    Push Auth Boarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,10 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)

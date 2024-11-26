60b0582c6efb8ffc2795123fe4a27ea39f16ce06
EliteHero <bagassatrio807@gmail.com>
2024-11-26 15:52:24 +0700

    Added Beranda Screen

---
+++ app/src/main/java/com/example/ajarinmobile/component/KelasLanjutanCard.kt
@@ -0,0 +1,20 @@
+package com.example.ajarinmobile.component
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.ajarinmobile.model.KelasLanjutan
+
+@Composable
+fun KelasLanjutanCard(modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+            contentColor = Color.Black
+        )
+    ) {
+
+    }
+}

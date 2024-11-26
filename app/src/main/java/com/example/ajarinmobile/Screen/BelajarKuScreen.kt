d1a90171380a1cb721bd0e305a5dce1a40324152
EliteHero <bagassatrio807@gmail.com>
2024-11-26 16:38:15 +0700

    Added placeholder images for screens

--- app/src/main/java/com/example/ajarinmobile/Screen/BelajarKuScreen.kt
+++ app/src/main/java/com/example/ajarinmobile/Screen/BelajarKuScreen.kt
@@ -1,12 +1,17 @@
 package com.example.ajarinmobile.Screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.example.ajarinmobile.R
 import com.example.ajarinmobile.component.AjarinBottomBar
 
 @Composable
@@ -15,6 +20,12 @@ fun BelajarKuScreen(navController: NavController) {
         bottomBar = { AjarinBottomBar(navController = navController) },
         containerColor = Color.White
     ) { innerPadding ->
-        Text("BelajarKu Screen", modifier = Modifier.padding(innerPadding))
+//        Text("BelajarKu Screen", modifier = Modifier.padding(innerPadding))
+        Image(
+            painter = painterResource(R.drawable.belajarku_placeholder),
+            contentDescription = "This is only a placeholder to see what the intended screen looks like",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+        )
     }
 }
